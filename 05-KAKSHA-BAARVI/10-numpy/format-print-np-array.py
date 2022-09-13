8c4232d8d3273a41af610c269928f7bf983606b2
Arun <jangirarun786@gmail.com>
2022-09-13 17:30:10 +0300

    now support formatting per column

--- 05-KAKSHA-BAARVI/10-numpy/format-print-np-array.py
+++ 05-KAKSHA-BAARVI/10-numpy/format-print-np-array.py
@@ -15,6 +15,8 @@ with np.printoptions(precision=3, suppress=False):
     print("Temporary format without supression \n",a,"\n")
 
 # Manual format
+print("Manual formatting")
+print("x.yf format means total \"x\" chars out of which \"y\" are decimals")
 
 print("Manual Formatting 10.3f with end=\"\"")
 for x in a:
@@ -27,3 +29,8 @@ for x in a:
     for i in x:
         print(f"{i:08.3f}",end=" ")
     print("")
+
+print("\nManual Formatting first two column with 7.2f and last three with 9.4f")
+print("x.yf format means total \"x\" chars out of which \"y\" are decimals")
+for i in range(len(a)):
+    print((2*"%7.2f"+3*"%9.4f") % tuple(a[i,:]))
